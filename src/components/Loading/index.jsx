d160343edd37c9920e439794449c1edf43da97b9
Vikram-Hegde <vikramhegde1440@gmail.com>
2022-12-09 22:38:26 +0530

    added loading spinner for login

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,14 @@
+import './style.scss'
+
+export default function Loading() {
+	return (
+		<div className="loading">
+			<div class="loading__ring">
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</div>
+		</div>
+	)
+}
--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -2,19 +2,25 @@ import './style.scss'
 import useAuth from '../../hooks/useAuth'
 import { useNavigate } from 'react-router-dom'
 import PageWrapper from '../../utils/PageWrapper'
+import { useState } from 'react'
+import Loading from '../../components/Loading'
 
 export default function Login() {
-	const [userAuth, setUserAuth] = useAuth()
+	const [_, setUserAuth] = useAuth()
 	const navigate = useNavigate()
+	const [loading, setLoading] = useState()
 
 	const handleSubmit = async (e) => {
 		e.preventDefault()
+		setLoading(true)
 		await setUserAuth.signin()
+		setLoading(false)
 		navigate('/')
 	}
 
 	return (
 		<PageWrapper className="login">
+			{loading && <Loading />}
 			<h1>Login Page</h1>
 			<form onSubmit={handleSubmit}>
 				<input type="text" name="inputName" />
