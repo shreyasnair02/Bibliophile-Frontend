fbe25943a61182f89851bd9d19dfc051fce8f92c
shreyasnair02 <SHREYAS.20201ISE0085@presidencyuniversity.in>
2022-12-14 15:50:05 +0530

    responsive home initial

--- src/routes/Home/Homepage/HomePage.jsx
+++ src/routes/Home/Homepage/HomePage.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
-import heroImg from '../../../assets/hero-img.png'
+import { NavLink } from 'react-router-dom'
+import heroImg1 from '../../../assets/hero-img.png'
+import heroImg2 from '../../../assets/hero-img2.png'
+import { IconBuildingStore } from '@tabler/icons'
 import './style.scss'
-
 export const HomePage = () => {
 	return (
 		<div className="home__container">
+			<div className="home__container__sellbook">
+				<NavLink to="/sellbook">
+					<button className="nav__btn nav__link nav__action purbtn">
+						<IconBuildingStore size={22} color="currentColor" />
+						Sell Books
+					</button>
+				</NavLink>
+			</div>
 			<div className="home__container__welcome">
 				<h3>paradise for book lovers</h3>
 				<h1>Bibliophile</h1>
-				<h2>Sharing knowledge has never been easier</h2>
-				<h2> Find and sell used books at lowest rates.</h2>
+				<h2>
+					<span id="content">Sharing knowledge has never been easier</span>{' '}
+					<span>Find and sell used books at lowest rates.</span>
+				</h2>
 			</div>
 			<div className="home__container__hero__container">
-				<img src={heroImg} alt="Bibliophilia" className="hero__img" />
+				<picture>
+					<source srcSet={heroImg1} media="(min-width:600px)" />
+					<img src={heroImg2} alt="Bibliophilia" className="hero__img" />
+				</picture>
 			</div>
 		</div>
 	)
