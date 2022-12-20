dbbb9f35706019303789ac6b190d2eeaebf1ab47
Vikram-Hegde <vikramhegde1440@gmail.com>
2022-12-20 20:04:02 +0530

    magic

--- src/routes/Bookshelf/index.jsx
+++ src/routes/Bookshelf/index.jsx
@@ -3,8 +3,20 @@ import Input from '../../components/Input'
 import './style.scss'
 import booksJson from '../../assets/books.json'
 import uuid4 from 'uuid4'
+import { useState } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+import { LayoutGroup } from 'framer-motion'
 
 export default function Bookshelf() {
+	const [books, setBooks] = useState(booksJson)
+
+	const handleChange = (e) => {
+		const searchQuery = e.target.value || ''
+		const filteredBooks = booksJson.filter((book) =>
+			book.title.toLowerCase().includes(searchQuery)
+		)
+		setBooks(filteredBooks)
+	}
 
 	return (
 		<PageWrapper className="bookshelf page">
@@ -12,24 +24,33 @@ export default function Bookshelf() {
 				<h4>Filter By</h4>
 			</div>
 			<div className="bookshelf__books">
-				<form className="bookshelf__form">
-					<Input
-						type="search"
-						className="bookshelf__search"
-						placeholder="Search for books..."
-					/>
-					{/* <button className=''>Search</button> */}
-				</form>
+				<Input
+					type="search"
+					className="bookshelf__search"
+					placeholder="Search for books..."
+					name="search"
+					onChange={handleChange}
+				/>
 				<div className="bookshelf__books-section">
-					{booksJson.map((book) => (
-						<div className="bookshelf__book" key={uuid4()}>
-							<img src={book.url} alt={book.title} />
-							<div className="bookshelf__book-info">
-								<p style={{ textDecoration: 'line-through'}}>₹{book.price + Math.ceil(Math.random() * 1000)}</p>
-								<h5>₹{book.price}</h5>
-							</div>
-						</div>
-					))}
+					<AnimatePresence mode="popLayout">
+						{books.map((book) => (
+							<LayoutGroup key={uuid4()}>
+								<motion.div
+									layout
+									transition={{ duration: 0.3 }}
+									className="bookshelf__book"
+								>
+									<img src={book.url} alt={book.title} />
+									<div className="bookshelf__book-info">
+										<p style={{ textDecoration: 'line-through' }}>
+											₹{book.price + 1000}
+										</p>
+										<h3>₹{book.price}</h3>
+									</div>
+								</motion.div>
+							</LayoutGroup>
+						))}
+					</AnimatePresence>
 				</div>
 			</div>
 			<div className="bookshelf__recommendation">
