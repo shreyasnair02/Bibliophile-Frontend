cf744f526aa600ddf5fe4251066e8bc8dca3d440
Vikram-Hegde <vikramhegde1440@gmail.com>
2022-12-20 20:59:10 +0530

    hmmmmm

--- src/routes/Bookshelf/index.jsx
+++ src/routes/Bookshelf/index.jsx
@@ -1,14 +1,39 @@
-import PageWrapper from '../../utils/PageWrapper'
-import Input from '../../components/Input'
-import './style.scss'
-import booksJson from '../../assets/books.json'
+import { AnimatePresence, LayoutGroup, motion } from 'framer-motion'
+import { useState, useEffect } from 'react'
 import uuid4 from 'uuid4'
-import { useState } from 'react'
-import { AnimatePresence, motion } from 'framer-motion'
-import { LayoutGroup } from 'framer-motion'
+import booksJson from '../../assets/books.json'
+import Input from '../../components/Input'
+import PageWrapper from '../../utils/PageWrapper'
+import './style.scss'
+
+function useDebounce(value, delay) {
+	// State and setters for debounced value
+	const [debouncedValue, setDebouncedValue] = useState(value)
+
+	useEffect(
+		() => {
+			// Update debounced value after delay
+			const handler = setTimeout(() => {
+				setDebouncedValue(value)
+			}, delay)
+
+			// Cancel the timeout if value changes (also on delay change or unmount)
+			// This is how we prevent debounced value from updating if value is changed ...
+			// .. within the delay period. Timeout gets cleared and restarted.
+			return () => {
+				clearTimeout(handler)
+			}
+		},
+		[value, delay] // Only re-call effect if value or delay changes
+	)
+
+	return debouncedValue
+}
 
 export default function Bookshelf() {
 	const [books, setBooks] = useState(booksJson)
+
+	const debouncedChanges = useDebounce(books, 200)
 
 	const handleChange = (e) => {
 		const searchQuery = e.target.value || ''
@@ -32,12 +57,12 @@ export default function Bookshelf() {
 					onChange={handleChange}
 				/>
 				<div className="bookshelf__books-section">
-					<AnimatePresence mode="popLayout">
-						{books.map((book) => (
+					<AnimatePresence>
+						{debouncedChanges.map((book) => (
 							<LayoutGroup key={uuid4()}>
 								<motion.div
 									layout
-									transition={{ duration: 0.3 }}
+									transition={{ duration: 0.2 }}
 									className="bookshelf__book"
 								>
 									<img src={book.url} alt={book.title} />
