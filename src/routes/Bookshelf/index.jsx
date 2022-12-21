1426c039d6bad4adaad49faeb08b6a3dfcf93d36
shreyasnair02 <SHREYAS.20201ISE0085@presidencyuniversity.in>
2022-12-21 15:17:17 +0530

    feat filters init commit

--- src/routes/Bookshelf/index.jsx
+++ src/routes/Bookshelf/index.jsx
@@ -1,15 +1,53 @@
+import { useState, useEffect } from 'react'
 import PageWrapper from '../../utils/PageWrapper'
 import Input from '../../components/Input'
 import './style.scss'
 import booksJson from '../../assets/books.json'
 import uuid4 from 'uuid4'
-
+import genres from './constants/genres.json'
 export default function Bookshelf() {
+	const [checkedState, setCheckedState] = useState(genres)
+	const [books, setBooks] = useState(booksJson)
+	let updatedBooks = [...booksJson]
+	useEffect(() => {
+		updatedBooks = books.filter((book) =>
+			checkedState.some((eachGenre) => {
+				return eachGenre.checked && book.genre.includes(eachGenre.genre)
+			})
+		)
+		setBooks(updatedBooks)
+	}, [checkedState])
+	const handleCheckstate = (position) => {
+		const updatedCheckedState = checkedState.map(
+			({ genre, checked }, index) => {
+				if (index === position) {
+					return { genre, checked: !checked }
+				} else {
+					return { genre, checked }
+				}
+			}
+		)
 
+		setCheckedState(updatedCheckedState)
+	}
 	return (
 		<PageWrapper className="bookshelf page">
 			<div className="bookshelf__filter">
 				<h4>Filter By</h4>
+				<div className="bookshelf__filter-section">
+					{checkedState.map((eachGenre, index) => (
+						<div key={uuid4()}>
+							<label htmlFor={eachGenre.genre}>{eachGenre.genre}</label>
+							<input
+								type="checkbox"
+								name="filter"
+								id={eachGenre.genre}
+								checked={eachGenre.checked}
+								onChange={() => handleCheckstate(index)}
+							/>
+						</div>
+					))}
+				</div>
 			</div>
 			<div className="bookshelf__books">
 				<form className="bookshelf__form">
@@ -21,11 +59,13 @@ export default function Bookshelf() {
 					{/* <button className=''>Search</button> */}
 				</form>
 				<div className="bookshelf__books-section">
-					{booksJson.map((book) => (
+					{books.map((book) => (
 						<div className="bookshelf__book" key={uuid4()}>
 							<img src={book.url} alt={book.title} />
 							<div className="bookshelf__book-info">
-								<p style={{ textDecoration: 'line-through'}}>₹{book.price + Math.ceil(Math.random() * 1000)}</p>
+								<p style={{ textDecoration: 'line-through' }}>
+									₹{book.price + Math.ceil(Math.random() * 1000)}
+								</p>
 								<h5>₹{book.price}</h5>
 							</div>
 						</div>
